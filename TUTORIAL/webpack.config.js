1e328e2f7b9d34cef279b1e46d4b46cb0598cd50
alan-serrano <alan.serrano.pch@gmail.com>
2020-04-18 15:47:52 -0400

    configuration of webpack

---
+++ TUTORIAL/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist/',
+        filename: 'bundle.js' 
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: [ 'transform-class-properties' ]
+                    }
+                }
+            }
+        ]
+    }
+};
